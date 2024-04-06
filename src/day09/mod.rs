575a04d4fbf729f3382a7c0bb1f28de3b6fd80ae
Rajas <rs.k.mahadule@gmail.com>
2024-04-06 15:29:25 +0530

    Created day 09

---
+++ src/day09/mod.rs
@@ -0,0 +1,2 @@
+pub mod day09a;
+pub mod day09b;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 
-mod day08;
+mod day09;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_path: &str = &args[1];
@@ -11,5 +11,5 @@ fn main() {
 }
 
 pub fn solve_day(input: String) -> u64 {
-    day08::day08b::solve(input)
+    day09::day09a::solve(input)
 }
